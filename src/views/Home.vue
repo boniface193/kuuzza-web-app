4fc2b1e9e428cfa8f5cce5f643fa01699acbb84b
abdulazeeza <abdulazeez@brandmobile.net>
2020-11-25 16:05:52 -0800

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-container fill-height>
+      <v-row justify="center" align="center">
+        <h1 class="blue--text">Welcome to Nova</h1>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
