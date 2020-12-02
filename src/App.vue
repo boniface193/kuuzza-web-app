05d7856d5f8c8342f2b1912b4dfd0aa24e90e7d0
boniface193 <bonifacedennis40@yahoo.com>
2020-12-02 06:29:11 -0800

    ft-calendar-filter-added

--- src/App.vue
+++ src/App.vue
@@ -31,4 +31,21 @@ body {
     text-transform: capitalize;
   }
 }
+
+*::-webkit-scrollbar {
+  width: 5px;
+}
+
+*::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: #5064cc;
+  outline: 1px solid slategrey;
+}
+
+:root {
+  scrollbar-color: #5064cc rgb(210, 210, 210) !important;
+}
 </style>
