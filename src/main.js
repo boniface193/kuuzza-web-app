b4c5a915c9033de26a87002b178572095fa7a0ec
boniface193 <bonifacedennis40@yahoo.com>
2020-12-07 01:40:44 -0800

    mockAPIsetup

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import vuetify from "./plugins/vuetify";
 import Chartkick from 'vue-chartkick'
 import Chart from 'chart.js'
 // mock server 
+<<<<<<< HEAD
 import { onboardServer } from '@/server/onboarding'
+=======
+import { onboardServer } from './server/onboarding.js'
+>>>>>>> 486481e... bg-fixed-mock-API-setup
 
 import VMdDateRangePicker from "@ikechukwu_boniface/vmddaterangepicker";
 
@@ -16,12 +20,16 @@ Vue.use(Chartkick.use(Chart))
 
 Vue.config.productionTip = false;
 
+<<<<<<< HEAD
 if (process.env.NODE_ENV === "development") {
   onboardServer;
 }
 
+=======
+>>>>>>> 486481e... bg-fixed-mock-API-setup
 new Vue({
   router,
+  onboardServer,
   store,
   vuetify,
   render: h => h(App)
