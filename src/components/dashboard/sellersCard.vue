c3099a3c92b4f6fe566184756dcaa0fa39a716aa
Ikechukwu Boniface <ikechukwu@brandmobile.net>
2021-01-01 19:50:55 +0000

    Merged in feature_NOVA_84 (pull request #65)
    Feature NOVA 84
    
    * rebasing
    
    * rebasing
    
    * rebasing
    
    * As a user i want to be able to view my balance page
    
    * As a user i want to be to view my settlement history
    
    * As a user i want to be to view my settlement history
    
    * switching branch
    
    * Merge branch 'develop' of https://bitbucket.org/Munachim/webapp into feature_NOVA_83
    
    * As a user i want to be able to filter and export both settlement history and transaction history
    
    * responsiveness adjustment

--- src/components/dashboard/sellersCard.vue
+++ src/components/dashboard/sellersCard.vue
@@ -1,153 +1,145 @@
 <template>
-  <v-container>
-    <div class="mx-lg-12 mx-md-5 mx-sm-5">
-      <v-img
-        src="../../assets/download.svg"
-        width="16"
-        height="15"
-        class="float-right"
-      ></v-img>
-      <v-spacer></v-spacer>
-      <router-link to="/seller" style="text-decoration: none; color: #2b2b2b">
-        <h4 class="float-left">
-          <v-icon class="mb-1 mr-3">mdi-chevron-left</v-icon> Sellers
-        </h4>
-      </router-link>
+  <div class="mx-lg-12 mx-md-5 mx-sm-5">
+    <v-img
+      src="../../assets/download.svg"
+      width="16"
+      height="15"
+      class="float-right"
+    ></v-img>
+    <v-spacer></v-spacer>
+    <router-link to="/seller" style="text-decoration: none; color: #2b2b2b">
+      <h4 class="float-left">
+        <v-icon class="mb-1 mr-3">mdi-chevron-left</v-icon> Sellers
+      </h4>
+    </router-link>
 
-      <div class="pa-2"></div>
-      <div class="my-8">
-        <div class="float-right">
-          <v-select
-            :items="item"
-            flat
-            solo
-            append-icon="mdi-chevron-down"
-            dense
-          >
-          </v-select>
-        </div>
-        <h1 class="heading--text">Emeka Mbakwe</h1>
+    <div class="pa-2"></div>
+    <div class="my-8">
+      <div class="float-right">
+        <v-select :items="item" flat solo append-icon="mdi-chevron-down" dense>
+        </v-select>
       </div>
+      <h1 class="heading--text">Emeka Mbakwe</h1>
+    </div>
 
-      <v-card color="white" elevation="0" class="py-2 rounded-lg mb-6">
-        <v-row>
-          <v-col md="4" v-for="sellers in sellerModels" :key="sellers.name">
-            <div class="d-flex">
-              <div class="d-flex justify-center mx-8">
-                <div
-                  :class="sellers.bg_color"
-                  class="rounded-pill pa-7 text-center"
-                ></div>
-                <div class="mx-4">
-                  <img
-                    :src="sellers.img"
-                    width="18.94px"
-                    height="25px"
-                    class="ml-percent mt-4"
-                  />
-                </div>
-              </div>
-              <div class="">
-                <p class="subtitles mb-0">{{ sellers.title }}</p>
-                <h4 class="headings">{{ sellers.amount }}</h4>
-              </div>
-            </div>
-          </v-col>
-        </v-row>
-      </v-card>
-
+    <v-card color="white" elevation="0" class="py-2 rounded-lg mb-6">
       <v-row>
-        <v-col md="8">
-          <v-card class="px-3" width="600px" elevation="0">
-            <v-row>
-              <v-col class="d-flex">
-                <v-img
-                  src="../../assets/img/laptop.png"
-                  class="grey lighten-3"
-                  width="180px"
-                ></v-img>
-              </v-col>
-              <v-col>
-                <h4 class="sellerscard">Order 100000</h4>
-                <h4
-                  v-for="items in productItems"
-                  :key="items.id"
-                  class="sellerscard"
-                >
-                  {{ items.productName }}:
-                  <span class="sellersCard" v-if="items.href"
-                    ><router-link to="/customers">{{
-                      items.brands
-                    }}</router-link></span
-                  >
-                  <span class="sellersCard" v-else>{{ items.brands }}</span>
-                </h4></v-col
-              >
-            </v-row>
-          </v-card>
-
-          <v-card class="px-3 my-4" width="600px" elevation="0">
-            <v-row>
-              <v-col class="d-flex">
-                <v-img
-                  src="../../assets/img/laptop.png"
-                  class="grey lighten-3"
-                  width="180px"
-                ></v-img>
-              </v-col>
-              <v-col>
-                <h4 class="sellerscard">Order 100000</h4>
-                <h4
-                  v-for="items in productItems"
-                  :key="items.id"
-                  class="sellerscard"
-                >
-                  {{ items.productName }}:
-                  <span class="sellersCard" v-if="items.href"
-                    ><router-link to="/customers">{{
-                      items.brands
-                    }}</router-link></span
-                  >
-                  <span class="sellersCard" v-else>{{ items.brands }}</span>
-                </h4></v-col
-              >
-            </v-row>
-          </v-card>
-        </v-col>
-
-        <v-col md="4" class="d-flex justify-end">
-          <v-card class="py-2" width="350px" elevation="0">
-            <div class="px-5">
-              <div class="my-5">
-                <p class="editSeller float-right">Edit</p>
-                <h4 class="sellerscard">Seller Overview</h4>
+        <v-col md="4" v-for="sellers in sellerModels" :key="sellers.name">
+          <div class="d-flex">
+            <div class="d-flex justify-center mx-8">
+              <div
+                :class="sellers.bg_color"
+                class="rounded-pill pa-7 text-center"
+              ></div>
+              <div class="mx-4">
+                <img
+                  :src="sellers.img"
+                  width="18.94px"
+                  height="25px"
+                  class="ml-percent mt-4"
+                />
               </div>
-              <p
-                v-for="sellersInfo in sellersOverview"
-                :key="sellersInfo.id"
-                class="sellersInfo mb-1"
-              >
-                <span>
-                  {{ sellersInfo }}
-                </span>
-              </p>
             </div>
-            <div class="hr"></div>
-            <div class="px-5">
-              <div class="my-5">
-                <p class="editSeller float-right">Change</p>
-                <h4 class="sellerscard">Address</h4>
-              </div>
-
-              <p class="w-25 pb-12">
-                129 Ademola Adetokunbo Crescent Wuse 2 FCT Abuja 900288 Nigeria
-              </p>
+            <div class="">
+              <p class="subtitles mb-0">{{ sellers.title }}</p>
+              <h4 class="headings">{{ sellers.amount }}</h4>
             </div>
-          </v-card>
+          </div>
         </v-col>
       </v-row>
-    </div>
-  </v-container>
+    </v-card>
+
+    <v-row>
+      <v-col md="8">
+        <v-card class="px-3" width="100%" elevation="0">
+          <v-row>
+            <v-col class="d-flex">
+              <v-img
+                src="../../assets/img/laptop.png"
+                class="grey lighten-3"
+                width="180px"
+              ></v-img>
+            </v-col>
+            <v-col>
+              <h4 class="sellerscard">Order 100000</h4>
+              <h4
+                v-for="items in productItems"
+                :key="items.id"
+                class="sellerscard"
+              >
+                {{ items.productName }}:
+                <span class="sellersCard" v-if="items.href"
+                  ><router-link to="/customers">{{
+                    items.brands
+                  }}</router-link></span
+                >
+                <span class="sellersCard" v-else>{{ items.brands }}</span>
+              </h4></v-col
+            >
+          </v-row>
+        </v-card>
+
+        <v-card class="px-3 my-4" width="100%" elevation="0">
+          <v-row>
+            <v-col class="d-flex">
+              <v-img
+                src="../../assets/img/laptop.png"
+                class="grey lighten-3"
+                width="180px"
+              ></v-img>
+            </v-col>
+            <v-col>
+              <h4 class="sellerscard">Order 100000</h4>
+              <h4
+                v-for="items in productItems"
+                :key="items.id"
+                class="sellerscard"
+              >
+                {{ items.productName }}:
+                <span class="sellersCard" v-if="items.href"
+                  ><router-link to="/customers">{{
+                    items.brands
+                  }}</router-link></span
+                >
+                <span class="sellersCard" v-else>{{ items.brands }}</span>
+              </h4></v-col
+            >
+          </v-row>
+        </v-card>
+      </v-col>
+
+      <v-col md="4" class="d-flex justify-end">
+        <v-card class="py-2" width="100%" height="98%" elevation="0">
+          <div class="px-5">
+            <div class="my-5">
+              <p class="editSeller float-right">Edit</p>
+              <h4 class="sellerscard">Seller Overview</h4>
+            </div>
+            <p
+              v-for="sellersInfo in sellersOverview"
+              :key="sellersInfo.id"
+              class="sellersInfo mb-1"
+            >
+              <span>
+                {{ sellersInfo }}
+              </span>
+            </p>
+          </div>
+          <div class="hr"></div>
+          <div class="px-5">
+            <div class="my-5">
+              <p class="editSeller float-right">Change</p>
+              <h4 class="sellerscard">Address</h4>
+            </div>
+
+            <p class="w-25 pb-12">
+              129 Ademola Adetokunbo Crescent Wuse 2 FCT Abuja 900288 Nigeria
+            </p>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -245,7 +237,7 @@ export default {
 
 .sellersCard {
   text-align: left;
-  font: normal normal normal 14px/17px "Product Sans";
+  font: normal normal normal 14px/32px "Product Sans";
   letter-spacing: 0px;
   color: #979797;
   opacity: 1;
