8d8b96af5666e48769093373a8a40916782d58a6
Ikechukwu Boniface <ikechukwu@brandmobile.net>
2021-01-05 08:24:26 +0000

    Merged in feature_NOVA_60 (pull request #67)
    Feature NOVA 60
    
    * rebasing
    
    * rebasing
    
    * rebasing
    
    * rebasing
    
    * rebasing
    
    * rebasing
    
    * rebasing
    
    * rebasing
    
    * adjusting dashboard
    
    * re-adjusting bar chart menu

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import vuetify from "./plugins/vuetify";
 import Chartkick from 'vue-chartkick'
 import Chart from 'chart.js'
 import VueApexCharts from 'vue-apexcharts'
-import moment from 'vue-moment'
 Vue.use(VueApexCharts)
 
 Vue.component('apexchart', VueApexCharts)
