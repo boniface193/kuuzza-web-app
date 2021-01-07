3c4373b4dfff0a6aeb445c1ea48e7931f09fad16
abdulazeeza <abdulazeez@brandmobile.net>
2021-01-07 15:13:47 -0800

    added naira sign and seperated money with comma

--- src/components/dashboard/productDetails.vue
+++ src/components/dashboard/productDetails.vue
@@ -121,7 +121,9 @@ export default {
       this.editInventory = false;
     },
     numberWithCommas(x) {
-      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      if (this.productDetails.name) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      }
     },
   },
 };
