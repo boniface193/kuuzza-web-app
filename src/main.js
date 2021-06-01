9ac378c5785277e522c43a01a42d16c894847b0d
abdulazeeza <abdulazeez@brandmobile.net>
2021-05-31 22:39:58 -0700

    add google analytics

--- src/main.js
+++ src/main.js
@@ -7,12 +7,18 @@ import Chartkick from 'vue-chartkick';
 import Chart from 'chart.js';
 import VueApexCharts from 'vue-apexcharts';
 import VueMeta from 'vue-meta';
+import VueAnalytics from 'vue-analytics';
 
 Vue.use(VueMeta)
 Vue.use(VueApexCharts)
 Vue.component('apexchart', VueApexCharts)
 Vue.use(Chartkick.use(Chart))
 Vue.use(require('vue-moment'));
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+  id: 'G-EF7LKNZ76Y',
+  router
+});
 
 
 Vue.config.productionTip = false;
