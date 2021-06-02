8559b4bf2a1b6cadab11818e6d4444f77e9216cd
abdulazeeza <abdulazeez@brandmobile.net>
2021-06-02 08:53:31 -0700

    fix bug on regress for recover password

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import Chart from 'chart.js';
 import VueApexCharts from 'vue-apexcharts';
 import VueMeta from 'vue-meta';
 import VueAnalytics from 'vue-analytics';
+import VueFacebookPixel from 'vue-facebook-pixel'
+ 
 
 Vue.use(VueMeta)
 Vue.use(VueApexCharts)
@@ -19,6 +21,13 @@ Vue.use(VueAnalytics, {
   id: 'G-EF7LKNZ76Y',
   router
 });
+Vue.use(VueFacebookPixel);
+Vue.analytics.fbq.init('197265088931338', {
+  em: 'user@mail.com'
+})
+Vue.analytics.fbq.event('ViewContent', {
+  content_name: 'Really Fast Running Shoes'
+})
 
 
 Vue.config.productionTip = false;
